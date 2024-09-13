5f45c2bcd27adf10cc1e4014e177d0feb677f96a
Nicolas <nicolasuliana10@gmail.com>
2024-09-12 23:13:52 -0300

    new .env

--- helpers/create-user-token.js
+++ helpers/create-user-token.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 const createUserToken = async(user, req, res) => {
     // create token
     const token = jwt.sign({
         name: user.name,
         id: user._id
-    }, "nossosecret")
+    }, `${process.env.JWT_SECRET}`)
 
     // return token
     res.status(200).json({
